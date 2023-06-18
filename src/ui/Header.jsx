c2e12b833a2aaa122950353473f60ce7fe6d6d80
Rizki Darmawan <rizdar.contact@gmail.com>
2023-06-18 21:25:12 +0700

    add layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header>
+      <Link to="/">Fast react Piza Co.</Link>
+
+      <p>Rizki</p>
+    </header>
+  );
+}
