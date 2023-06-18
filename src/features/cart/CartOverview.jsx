bd152a57597772922c9a2a82f00390b9c33c7183
Rizki Darmawan <rizdar.contact@gmail.com>
2023-06-18 21:26:53 +0700

    small adjustment for layout

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,4 +1,4 @@
-import Link from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function CartOverview() {
   return (
@@ -11,5 +11,4 @@ function CartOverview() {
     </div>
   );
 }
-
 export default CartOverview;
