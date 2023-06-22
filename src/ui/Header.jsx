430aea146cf6a885e72e5309c3be3caf0c6d131d
Rizki Darmawan <rizdar.contact@gmail.com>
2023-06-22 20:36:05 +0700

    add search order component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 export default function Header() {
   return (
     <header>
       <Link to="/">Fast react Piza Co.</Link>
-
+      <SearchOrder />
       <p>Rizki</p>
     </header>
   );
